bbee790a3d318102c2f4e1252f73eafb19d04835
Nicolas <nizalasik@gmail.com>
2024-07-25 13:40:05 -0300

    Projeto 90%

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const tarefaInput = document.getElementById("tarefa");
 const adicionarBotao = document.getElementById("adicionar");
 const listaTarefas = document.getElementById("tarefas");
+const notification = document.getElementById("notification");
 
 // Event Listeners
 adicionarBotao.addEventListener("click", adicionarTarefa);
@@ -16,10 +17,13 @@ function adicionarTarefa() {
     if (tarefaTexto.trim() !== "") {
         const novaTarefa = document.createElement("li");
         novaTarefa.innerHTML = `
-        <p class="task"><img class="check" src="_assets/confirm.svg" alt=""> ${tarefaTexto} <img class="excluir check" src="_assets/cancel.svg" alt=""></p>
+            <p class="task"><img class="check" src="_assets/confirm.svg" alt=""> ${tarefaTexto} <img class="excluir check" src="_assets/cancel.svg" alt=""></p>
         `;
         listaTarefas.appendChild(novaTarefa);
         tarefaInput.value = "";
+        notification.style.display = "none"; // Oculta a notificação quando a tarefa é adicionada
+    } else {
+        notification.style.display = "block"; // Exibe a notificação quando o campo está vazio
     }
 }
 
